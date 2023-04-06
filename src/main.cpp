39e081b0227a9694ab94187e06161d1513c7fb68
juthomas <juthomas@student.42.fr>
2023-04-06 17:41:36 +0200

    [ADD] UDP commands tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,119 +1,36 @@
-#include <Arduino.h>
-#include <PS4Controller.h>
-#define MOTOR_1_A 2
-#define MOTOR_1_B 15
-#define MOTOR_2_A 13
-#define MOTOR_2_B 12
-#define FORWARD_CHANNEL 1
-#define BACKWARD_CHANNEL 2
-#define MOTOR_FREQUENCY 5000
-#define MOTOR_RESOLUTION 8
+#include <WiFi.h>
+#include <WiFiUDP.h>
 
-#define FRONT_LIGHT_1 21
-#define FRONT_LIGHT_2 22
-#define FRONT_LIGHT_CHANNEL 3
+const char* ssid = "ESP32WIFI";
+const char* password = "44448888";
+WiFiUDP udp;
 
-uint8_t front_light_power = 0;
-
-void setup()
-{
+void setup() {
   Serial.begin(115200);
-  Serial.printf("Hello world\n");
 
-  PS4.begin((char *)"b0:be:83:7f:20:2d");
-  pinMode(MOTOR_1_A, OUTPUT);
-  pinMode(MOTOR_1_B, OUTPUT);
+  // Configuration de l'ESP32 en tant que point d'accès
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("ESP32AP", "motdepasse");
+  Serial.print("Point d'accès créé, adresse IP: ");
+  Serial.println(WiFi.softAPIP());
 
-  ledcSetup(FORWARD_CHANNEL, MOTOR_FREQUENCY, MOTOR_RESOLUTION);
-  ledcSetup(BACKWARD_CHANNEL, MOTOR_FREQUENCY, MOTOR_RESOLUTION);
-  ledcSetup(FRONT_LIGHT_CHANNEL, MOTOR_FREQUENCY, MOTOR_RESOLUTION);
-  ledcAttachPin(MOTOR_2_B, FORWARD_CHANNEL);
-  ledcAttachPin(MOTOR_2_A, BACKWARD_CHANNEL);
-  ledcAttachPin(FRONT_LIGHT_1, FRONT_LIGHT_CHANNEL);
-  ledcAttachPin(FRONT_LIGHT_2, FRONT_LIGHT_CHANNEL);
+  // Démarrage du serveur UDP sur le port 1234
+  udp.begin(12345);
+  Serial.println("Serveur UDP démarré sur le port 1234");
 }
 
-void test_loop()
-{
-  digitalWrite(MOTOR_1_A, HIGH);
-  digitalWrite(MOTOR_1_B, LOW);
-  delay(500);
-  digitalWrite(MOTOR_1_A, LOW);
-  digitalWrite(MOTOR_1_B, HIGH);
-  delay(500);
-  digitalWrite(MOTOR_1_A, LOW);
-  digitalWrite(MOTOR_1_B, LOW);
-  delay(500);
-
-  for (int dutyCycle = 0; dutyCycle <= 30; dutyCycle++)
-  {
-    ledcWrite(FORWARD_CHANNEL, dutyCycle);
-    delay(65);
-  }
-  ledcWrite(FORWARD_CHANNEL, 0);
-
-  for (int dutyCycle = 0; dutyCycle <= 30; dutyCycle++)
-  {
-    // changing the LED brightness with PWM
-    ledcWrite(BACKWARD_CHANNEL, dutyCycle);
-    delay(65);
-  }
-  ledcWrite(BACKWARD_CHANNEL, 0);
-
-  delay(500);
-}
-
-int r = 255;
-int g = 0;
-int b = 0;
-// Calculates the next value in a rainbow sequence
-void nextRainbowColor()
-{
-  if (r > 0 && b == 0)
-  {
-    r--;
-    g++;
-  }
-  if (g > 0 && r == 0)
-  {
-    g--;
-    b++;
-  }
-  if (b > 0 && g == 0)
-  {
-    r++;
-    b--;
-  }
-}
-
-void loop()
-{
-
-  if (PS4.isConnected())
-  {
-
-    ledcWrite(FORWARD_CHANNEL, PS4.R2Value());
-    ledcWrite(BACKWARD_CHANNEL, PS4.L2Value());
-
-    digitalWrite(MOTOR_1_A, PS4.Left() ? HIGH : LOW);
-    digitalWrite(MOTOR_1_B, PS4.Right() ? HIGH : LOW);
-    ledcWrite(FRONT_LIGHT_CHANNEL, front_light_power);
-
-    if (PS4.Circle() && front_light_power < 254)
-    {
-      front_light_power++;
+void loop() {
+  // Lire les données UDP disponibles
+  int packetSize = udp.parsePacket();
+  if (packetSize) {
+    // Si des données sont disponibles, les lire et les afficher sur la sortie standard
+    char buffer[packetSize];
+    int len = udp.read(buffer, packetSize);
+    if (len > 0) {
+      buffer[len] = 0;
+      Serial.print("Message UDP reçu: ");
+      Serial.println(buffer);
     }
-    if (PS4.Cross() && front_light_power > 0)
-    {
-      front_light_power--;
-    }
-
-    Serial.printf("INPUTS : TRIG %d BATT %d\n", PS4.R2Value(), PS4.Battery());
-    // nextRainbowColor();
-    //     PS4.setLed(r, g, b);
-    //     // PS4.setRumble(PS4.RStickY(), PS4.RStickX());
-    //     PS4.sendToController();
-    //     // PS4.setLed(r, g, b);
-    delay(10);
   }
+  delay(10);
 }
